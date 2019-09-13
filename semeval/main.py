940df7bd240c3a6c7d7f31ee7e5c622643e541b8
jzheadley <headleyjz@vcu.edu>
2019-09-12 21:11:51 -0400

    finished off the emoji sentiment mapping function

--- semeval/main.py
+++ semeval/main.py
@@ -3,17 +3,15 @@ from pre_processing import cleaning
 import re
 import emoji
 
+# https://stackoverflow.com/a/43146653/5472958
 def extract_emojis(str):
   return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
 
-def calculate_emoji_sentiments(data):
+def calculate_emoji_sentiments(data, threshold=10):
     regex = re.compile(r'\d+(.*?)[\u263a-\U0001f645]')
     emoji_sentiments={}
     for instance in data:
-        # for token in instance['tokens']:
-            # print(token)
         emojis=extract_emojis(instance['tweet'])
-        # emojis = re.findall(r"\\u\d\d\d\d",token)
         sentiment = 1 if  instance['sentiment']=='positive' else -1 if instance['sentiment']=='negative' else 0 
         for emoji in emojis:
             if emoji not in emoji_sentiments:
@@ -22,9 +20,11 @@ def calculate_emoji_sentiments(data):
                 emoji_sentiments[emoji] = emoji_sentiments[emoji] + sentiment
     sortedEmojis = (sorted((value, key) for (key,value) in emoji_sentiments.items()))
     # print(sortedEmojis)
-    threshold = 10
-    print([x for x in sortedEmojis if x[0] > threshold or x[0] <  -1 * threshold])
-    return emoji_sentiments
+    thresholded_emojis = [x for x in sortedEmojis if x[0] > threshold or x[0] <  -1 * threshold]
+    # print(thresholded_emojis)
+    mapped_emojis = [('positive',emoji[1]) if emoji[0] > 0 else ('negative',emoji[0])  for emoji in thresholded_emojis]
+    print(mapped_emojis)
+    return mapped_emojis
 
 if __name__ =='__main__':
     # parse_conll_to_json('train_conll_spanglish.txt','tweets_train.json')
